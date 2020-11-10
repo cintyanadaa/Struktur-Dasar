33183efe1557c5fc81a3fc623d85c4a4c4aab54d
cintyanadaa <cintyanadaa17@gmail.com>
2020-11-10 17:25:27 +0700

    publish file index

---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic;
+
+import com.dicoding.javafundamental.basic.kendaraan.kereta;
+import com.dicoding.javafundamental.basic.kendaraan.mobil;
+import com.dicoding.javafundamental.basic.kendaraan.motor;
+import com.dicoding.javafundamental.basic.musik.gitar;
+
+/**
+ *
+ * @author Sindy
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Haloo Semesta");
+ gitar.bunyi();
+ 
+ mobil.jumlahBan();
+ motor.jumlahBan();
+ kereta.jumlahBan();
+ 
+    }
+    
+}
